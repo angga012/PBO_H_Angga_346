3ff907195a825849dc234ee16bb0a507022a18d3
angga012 <bimacity318@gmail.com>
2025-03-05 10:31:19 +0700

    Ini commit pertama

---
+++ src/modul1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class modul1 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("masukan nama:");
+      String nama = scan.nextLine();
+        System.out.print("masukan jenis kelamin:");
+      String gender = scan.next();
+        System.out.print("masukan tahun lahir:");
+        int tahunlahir = scan.nextInt();
+
+
+    }
+}
